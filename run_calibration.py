c5ed58c1d4bab167c0f1bd7f2c4f6dd27a618e37
James Gowans <gowans.james@gmail.com>
2015-05-25 17:36:42 +0200

    Calibration running script added
    The script sets up the interfaces and a callibrator, and then calls
    calibration routines. Currently only offset calibration.

---
+++ run_calibration.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import corr
+import snapshot
+import iadc
+import calibrator
+import logging
+import time
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+fpga = corr.katcp_wrapper.FpgaClient('localhost')
+time.sleep(1)
+print(fpga.est_brd_clk())
+snap_I = snapshot.Snapshot(fpga, 'snapshot', logger)
+snap_Q = snapshot.Snapshot(fpga, 'snapshot1', logger)
+iadc = iadc.IAdc(fpga, zdok_n = 0, mode = 'indep', logger=logger)
+iadc.write_all_registers()
+cal = calibrator.Calibrator(iadc, snap_I, snap_Q, logger)
+cal.run_offset_cal()
