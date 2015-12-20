bb5587e4f17d76c94de071c54b9e2d912f8aaef8
James Gowans <gowans.james@gmail.com>
2015-12-20 11:57:58 +0200

    Calibrated ADC1

--- run_calibration.py
+++ run_calibration.py
@@ -9,7 +9,7 @@ from colorlog import ColoredFormatter
 import time
 from directionFinder_backend.correlator import Correlator
 
-ZDOK_N = 0
+ZDOK_N = 1
 
 
 if __name__ == '__main__':
@@ -26,10 +26,12 @@ if __name__ == '__main__':
     adr = AdcDataWrapper(correlator, ZDOK_N, logger = logger.getChild('adr'))
     adr.resample()
     iadc = iadc.IAdc(fpga, zdok_n = ZDOK_N, mode = 'indep', logger=logger.getChild('iadc'))
-    iadc.registers.get_from_file('registers.json')
+    iadc.registers.get_from_file('registers_{zdok_n}.json'.format(zdok_n = ZDOK_N))
     iadc.write_all_registers()
+    iadc.set_cal_mode('no_cal')
+    time.sleep(0.5)
     cal = calibrator.Calibrator(iadc, adr, interleaved=False, logger=logger.getChild('calibrator'))
-    cal.run_offset_cal()
+    #cal.run_offset_cal()
     cal.run_phase_difference_cal()
     #cal.run_analogue_gain_cal()
-    iadc.registers.save_to_file('registers.json')
+    iadc.registers.save_to_file('registers_{zdok_n}.json'.format(zdok_n = ZDOK_N))
