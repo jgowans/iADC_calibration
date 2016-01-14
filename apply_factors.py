f823abe18f24804e83aaba33c2faecdfcefe648f
James Gowans <gowans.james@gmail.com>
2016-01-15 01:21:37 +0200

    Add a small sleep

--- apply_factors.py
+++ apply_factors.py
@@ -4,6 +4,7 @@ import corr
 import logging
 from colorlog import ColoredFormatter
 from iadc import IAdc
+import time
 
 if __name__ == '__main__':
     logger = logging.getLogger('main')
@@ -14,8 +15,10 @@ if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
 
     fpga = corr.katcp_wrapper.FpgaClient('localhost', timeout=10)
+    time.sleep(1)
     logger.info("FPGA running at: {f}".format(f = fpga.est_brd_clk()))
     for zdok_n in (0, 1):
         iadc = IAdc(fpga, zdok_n = zdok_n, mode = 'indep', logger=logger.getChild('iadc'))
         iadc.registers.get_from_file('registers_{zdok_n}.json'.format(zdok_n = zdok_n))
+        iadc.set_cal_mode('no_cal')
         iadc.write_all_registers()
